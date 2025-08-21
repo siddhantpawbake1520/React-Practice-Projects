8b9a48e823f2cf1a20ba810582170f615f4a15e8
Siddhant <sidpawbake15@gmail.com>
2025-08-21 16:51:05 +0530

    ToDo using Redux

---
+++ ToDo(Redux)/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import AddTodo from './components/addtodo'
+import Todos from './components/todo'
+
+function App() {
+  
+  return (
+    <>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
