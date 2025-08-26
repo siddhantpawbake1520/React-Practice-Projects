53022d255405f424be3b38e5aa00f2d3c0921219
Siddhant <sidpawbake15@gmail.com>
2025-08-26 14:30:25 +0530

    Update#1

--- BlogWebsite/src/App.jsx
+++ BlogWebsite/src/App.jsx
@@ -1,14 +1,53 @@
-import { useState } from 'react'
-
-import './App.css'
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./features/authSlice";
+import { Footer, Header } from "./component";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // ✅ Call Appwrite to check if session exists
+    authService.getCurrentUser()
+      .then((userData) => {
+            console.log("Current User from Appwrite:", userData)
+        if (userData) {
+          // store full user object in redux
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch(() => {
+        // In case of error, clear session
+        dispatch(logout());
+      })
+      .finally(() => setLoading(false));
+  }, [dispatch]);
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-400">
+        <h1 className="text-xl font-bold">Checking session...</h1>
+      </div>
+    );
+  }
+
   return (
-    <>
-      <h1>BlogApp</h1>
-    </>
-  )
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- BlogWebsite/src/store/store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import authSlice from '../features/authSlice';
-
-const store=configureStore({
-    reducer:{
-            auth : authSlice,
-    }
-})
-export default store
